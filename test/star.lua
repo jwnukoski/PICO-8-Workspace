bdc66a14f1493558e1fcd24f78fa169ffdd6c72f
jwnukoski <jwnukoski@protonmail.com>
2024-05-22 07:33:26 -0400

    stars

--- test/star.lua
+++ test/star.lua
@@ -10,7 +10,7 @@ function Star.new(x, y)
     
     -- Cheap distance effect
     self.speed = flr(rnd(3)) + 1
-
+    
     self.color = COLOR.DRK_GRY
     if (self.speed == 2) then
         self.color = COLOR.LGT_GRY
