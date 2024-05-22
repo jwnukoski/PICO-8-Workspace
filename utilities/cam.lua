fdc77b0e0bea1239af703903ce3c06c42a7358f0
jwnukoski <jwnukoski@protonmail.com>
2024-05-22 00:03:05 -0400

    graphics

--- utilities/cam.lua
+++ utilities/cam.lua
@@ -1,8 +1,8 @@
--- Global camera object with helpers
+-- Global camera object
 CAM = {
     x = 0,
     y = 0,
-    
+
     -- Move the camera by x and y amounts
     move = function(self, x, y)
         self:set(self.x + x, self.y + y)
