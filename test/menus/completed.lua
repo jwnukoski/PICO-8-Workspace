4bf4b052693f063959e90103fcb40fe04bbaca6e
jwnukoski <jwnukoski@protonmail.com>
2024-06-02 16:23:56 -0400

    completed

--- test/menus/completed.lua
+++ test/menus/completed.lua
@@ -12,9 +12,11 @@ end
 function MenuGameCompleted:draw()
     rectfill(0, 0, SCREEN.WIDTH, SCREEN.HEIGHT, COLOR.BLK)
 
-    print("Game Completed!", 0 , SCREEN.HEIGHT / 5, COLOR.GRN)
-    print("Thanks for playing!", 0, SCREEN.HEIGHT / 4, COLOR.WHT)
-    print("Press " .. ASCII.KEY_O .. " to restart", 0, SCREEN.HEIGHT / 2, COLOR.WHT)
+    print("Game Completed!", 0 , 32, COLOR.GRN)
+    print("Thanks for playing!", 0, 40, COLOR.WHT)
+    print("Press " .. ASCII.KEY_O .. " to restart", 0, 48, COLOR.WHT)
+    
+    print("Your score was: " .. SCORE, 0, 56, COLOR.WHT)
 
     print("DEV SITE: jwnukoski.com", 0 , SCREEN.HEIGHT - 16, COLOR.WHT)
     print("github.com/jwnukoski", 0, SCREEN.HEIGHT - 8, COLOR.WHT)
